c03f0ef8dbc6da7821fdd0eb8c86f8fc3091cbea
dheekshac <dheekshac07@gmail.com>
2025-09-12 23:07:30 +0530

    Create solution.cpp
    #include <vector>
    using namespace std;
    std::vector<int> invert(std::vector<int> values)
    {
        vector<int> result;
        for(int i=0;i<values.size();i++){
        
            result.push_back(values[i]*(-1));
          }
        return result;
    }

---
+++ arrays_cpp_easy/Invert values/solution.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+using namespace std;
+std::vector<int> invert(std::vector<int> values)
+{
+    vector<int> result;
+    for(int i=0;i<values.size();i++){
+    
+        result.push_back(values[i]*(-1));
+      }
+    return result;
+}
