516efc31bf550ecb83f0056333f3d58544dbb72d
dheekshac <dheekshac07@gmail.com>
2025-09-15 00:14:43 +0530

    Create solution.cpp
    #include <bits/stdc++.h>
    using namespace std;
    int find_largest(vector<int>arr,int n){
      int largest=arr[0];
      for(int i=0;i<n;i++){
          if(arr[i]>largest){
              largest=arr[i];
          }
      }
      return largest;
    }
    
    int main(){
      int n;cin>>n;
    	vector<int>arr(n);
    	for(int i=0;i<n;i++){
    	    cin>>arr[i];
    	}
      int largest= find_largest(arr,n);
      cout<<largest;
    }

---
+++ dsa_arrays/find_largest_number_optimal/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int find_largest(vector<int>arr,int n){
+  int largest=arr[0];
+  for(int i=0;i<n;i++){
+      if(arr[i]>largest){
+          largest=arr[i];
+      }
+  }
+  return largest;
+}
+
+int main(){
+  int n;cin>>n;
+	vector<int>arr(n);
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+  int largest= find_largest(arr,n);
+  cout<<largest;
+}
