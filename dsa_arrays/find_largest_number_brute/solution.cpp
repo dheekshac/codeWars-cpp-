31c83bba9c8c54f865324c248953e47bef4ea128
dheekshac <dheekshac07@gmail.com>
2025-09-15 00:17:14 +0530

    Update solution.cpp
    #include <bits/stdc++.h>
    using namespace std;
    int find_largest(vector<int>arr,int n){
        sort(arr.begin(),arr.end());
        return arr[n-1];
    }
    
    int main(){
        int n;cin>>n;
        int d;cin>>d;
    	vector<int>arr(n);
    	for(int i=0;i<n;i++){
    	    cin>>arr[i];
    	}
        int largest= find_largest(arr,n);
        cout<<largest;
    }

--- dsa_arrays/find_largest_number_brute/solution.cpp
+++ dsa_arrays/find_largest_number_brute/solution.cpp
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int find_largest(vector<int>arr,int n){
-    int largest=arr[0];
-    for(int i=0;i<n;i++){
-        if(arr[i]>largest){
-            largest=arr[i];
-        }
-    }
-    return largest;
+    sort(arr.begin(),arr.end());
+    return arr[n-1];
 }
 
 int main(){
-  int n;cin>>n;
+    int n;cin>>n;
+    int d;cin>>d;
 	vector<int>arr(n);
 	for(int i=0;i<n;i++){
 	    cin>>arr[i];
 	}
-  int largest= find_largest(arr,n);
-  cout<<largest;
+    int largest= find_largest(arr,n);
+    cout<<largest;
 }
